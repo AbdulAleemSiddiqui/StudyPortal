eaa44f406f267250deb7902726a207c0dd10d4dd
smfasihaly <smfasihali@gmail.com>
2020-04-16 15:25:19 +0500

    withoun web config

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -11,7 +11,7 @@ namespace FYP1.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
+        // GET: Loging
         public ActionResult StudentLogin()
         {
             return View();
